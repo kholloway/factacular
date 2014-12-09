9b751539d973826a1eac9c6bcfe6938866aa5795
Kent Holloway <drizit@gmail.com>
2014-12-09 14:33:29 -0600

    Switch padding to right instead of left. Add sorting for fact name when calling node-facts

--- cmd_node_facts.go
+++ cmd_node_facts.go
@@ -6,9 +6,25 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/codegangsta/cli"
+	"github.com/temal-/go-puppetdb"
 )
+
+type ByName []puppetdb.FactJson
+
+func (slice ByName) Len() int {
+	return len(slice)
+}
+
+func (slice ByName) Less(i, j int) bool {
+	return slice[i].Name < slice[j].Name
+}
+
+func (slice ByName) Swap(i, j int) {
+	slice[i], slice[j] = slice[j], slice[i]
+}
 
 func nodeFacts(c *cli.Context) {
 	if c.Args().First() == "" {
@@ -23,7 +39,9 @@ func nodeFacts(c *cli.Context) {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	sort.Sort(ByName(resp))
 	for _, element := range resp {
-		fmt.Printf("%30v %25v %v\n", c.Args().First(), element.Name, element.Value)
+		fmt.Printf("%-30v %-25v %v\n", c.Args().First(), element.Name, element.Value)
 	}
 }
